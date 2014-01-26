5485eb7409bd2273fb5b1756191e114bbdc4630c
Anthony Ramirez <amboxer21@yahoo.com>
2014-01-26 01:45:45 -0500

    Update GmailCracker.pl

--- GmailCracker.pl
+++ GmailCracker.pl
@@ -1,15 +1,22 @@
+
 use strict;
 
 use Tk;
+require Tk::FileSelect;
 
 my $mw = MainWindow->new;
    $mw->title( 'Gmail Cracker' );
    $mw->geometry( '400x400' );
 my $EntryLabel = $mw->Label( -text => 'User Name ' )->pack( -side => 'left', -anchor => 'w', );      
 my $ListButton = $mw->Button(-text => "Get List", 
-		             -command => sub { $mw->chooseDirectory; }, )->pack( -side => 'right', -anchor => 'e', -padx => '5', -pady => '5'  );
+		             -command => \&choose_dir )->pack( -side => 'right', -anchor => 'e', -padx => '5', -pady => '5'  );
 		             
 my $UserNameEntry = $mw->Entry( -background => 'WHITE', -width => '28', )->pack( -side => 'left', -anchor => 'e', );		             
-#my $Scrolled = $mw->Scrolled( 'Text', )->pack( -side => 'top', );
+
+sub choose_dir {
+my $FileSelector = $mw->FileSelect( -directory => '/' );
+my $File = $FileSelector->Show;
+print $File;
+};
 
 MainLoop;
