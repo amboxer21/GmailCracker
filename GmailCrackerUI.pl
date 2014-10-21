2d91876aedb1565eb3fbf9b2d3f5f5f5ca5c8e3b
Chankey Pathak <chankey007@gmail.com>
2014-10-21 09:35:05 +0530

    Update GmailCrackerUI.pl

--- GmailCrackerUI.pl
+++ GmailCrackerUI.pl
@@ -4,8 +4,8 @@ use Tk;
 require Tk::FileSelect;
 
 my $mw = MainWindow->new;
-   $mw->title( 'Gmail Cracker' );
-   $mw->geometry( '400x400' );
+$mw->title( 'Gmail Cracker' );
+$mw->geometry( '400x400' );
 my $EntryLabel = $mw->Label( -text => 'User Name ' )->pack( -side => 'left', -anchor => 'w', );      
 my $ListButton = $mw->Button( -text => "Get List", 
 		              -command => \&choose_dir )->pack( -side => 'right', -anchor => 'e', -padx => '5', -pady => '5'  );
